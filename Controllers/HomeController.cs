1a33dab6c8ed5b29f0b32aea40d7a225397cefb3
Sharma, Mohan (Noida) <Mohan.Sharma@fiserv.com>
2021-06-25 18:41:55 +0530

    test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,21 +6,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using DemoDotnetCoreApplication.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace DemoDotnetCoreApplication.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _congi;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration congi )
         {
             _logger = logger;
+            _congi = congi;
         }
 
         public IActionResult Index()
         {
-            return View();
+            
+            return View(_congi["MyKey"].ToString());
         }
 
         public IActionResult Privacy()
